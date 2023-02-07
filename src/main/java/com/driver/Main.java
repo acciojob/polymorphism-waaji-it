8962bcc09798189e10d7fa1a09e31c7c322f230d
waaji-it <abdul.wajid.sahib@gmail.com>
2023-02-08 01:58:57 +0530

    First Submission Of Assignment

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,16 @@
 package com.driver;
 
 public class Main {
+   public void main(String[] args) {
+         Product p = new Product();
+         p.product(0,0);
+         p.product(0,0,0);
+         p.product(0.0,0.0);
 
+   }
+   class Product{
+      public int product(int x, int y) {return 0;}
+      public int product(int x, int y, int z) {return 0;}
+      public double product(double x, double y) {return 0;}
+   }
 }
