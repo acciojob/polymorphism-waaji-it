e0b2b0cc6e9cf383640e1a3bd2f0af4fb3da72d1
waaji-it <abdul.wajid.sahib@gmail.com>
2023-02-11 02:08:59 +0530

    Last try

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,7 +1,7 @@
 package com.driver;
 
 public class Main {
-    static class Product{
+    public static class Product{
         public int product(int x, int y) {return x*y;}
         public int product(int x, int y, int z) {return x*y*z;}
         public double product(double x, double y) {return x*y;}
