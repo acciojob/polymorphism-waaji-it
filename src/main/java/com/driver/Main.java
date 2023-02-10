a4e81a155260bf1aa6757086f907db28c261eb54
waaji-it <abdul.wajid.sahib@gmail.com>
2023-02-11 02:05:17 +0530

    Third Commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,17 +1,16 @@
 package com.driver;
 
 public class Main {
-   public void main(String[] args) {
+    static class Product{
+        public int product(int x, int y) {return x*y;}
+        public int product(int x, int y, int z) {return x*y*z;}
+        public double product(double x, double y) {return x*y;}
+    }
+   public static void main(String[] args) {
          Product p = new Product();
-         p.product(0,0);
-         p.product(0,0,0);
-         p.product(0.0,0.0);
+         p.product(2,3);
+         p.product(2,3,4);
+         p.product(2.5,3.5);
 
    }
-    public class Product{
-        public int product(int x, int y) {return 0;}
-        public int product(int x, int y, int z) {return 0;}
-        public double product(double x, double y) {return 0;}
-    }
-
 }
